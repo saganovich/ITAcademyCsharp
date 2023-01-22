6056b28a0f2d53e5e3f1ff73a2cd239510eefd3d
saganovich <saganovich.alexander@yandex.by>
2023-01-22 22:51:12 +0300

    Task completed

---
+++ L2_Conversion/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace L2_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit conversion
+            byte tempByte = 255;
+            ushort tempUShort = tempByte;
+            
+            sbyte tempSByte = -128;
+            short tempShort = tempSByte;
+
+            tempShort = 32767;
+            int tempInt = tempShort;
+
+            tempInt += 10000000;
+            long tempLong = tempInt;
+
+            tempInt = tempSByte;
+
+            //Explicit conversion
+            int tempIntA = 128;
+            int tempIntB = 127;
+            tempByte = (byte)(tempIntA + tempIntB);
+            
+            char tempChar = 'A';
+            tempInt = (int)tempChar;
+
+            tempInt = 32767;
+            tempShort = (short)tempInt;
+
+            tempIntA = 0b10000000;
+            tempIntB = 0b1111111;
+            tempByte = (byte)(tempIntA + tempIntB);
+
+            tempIntA = 0x07F;
+            tempIntB = 0x080;
+            tempByte = (byte)(tempIntA + tempIntB);
+
+            //Boxing and unboxing
+            int tempIntY = 1;
+            object tempObj = tempIntY;      //Boxing
+            int TempIntZ = (int)tempObj;    //Unboxing
+
+            //Implicitly typed variable
+            var tempVarA = 2.5;
+            var tempVarB = 5.5;
+            var tempVarC = tempVarA * tempVarB;
+
+            //tempVarC = "Hello,world!";
+            /*Какой тип будет закреплен за неявно типизированной переменной определяется в момент её инициализации.
+             * Этот тип остаётся закрепленным за ней до конца существования переменной.*/
+        }
+    }
+}
---
+++ L2_Types/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace L2_Types
+{
+    internal class Program
+    {
+        static void InputValue(string type)
+        {
+            try
+            {
+                switch(type)
+                {
+                    case "bool":
+                        Console.Write("Enter value: ");
+                        bool tBool = Convert.ToBoolean(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tBool);
+                        break;
+                    case "byte":
+                        Console.Write("Enter value: ");
+                        byte tByte = Convert.ToByte(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tByte);
+                        break;
+                    case "sbyte":
+                        Console.Write("Enter value: ");
+                        sbyte tSByte = Convert.ToSByte(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tSByte);
+                        break;
+                    case "short":
+                        Console.Write("Enter value: ");
+                        short tShort = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tShort);
+                        break;
+                    case "ushort":
+                        Console.Write("Enter value: ");
+                        ushort tUShort = Convert.ToUInt16(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tUShort);
+                        break;
+                    case "int":
+                        Console.Write("Enter value: ");
+                        int tInt= Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tInt);
+                        break;
+                    case "uint":
+                        Console.Write("Enter value: ");
+                        uint tUInt = Convert.ToUInt32(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tUInt);
+                        break;
+                    case "long":
+                        Console.Write("Enter value: ");
+                        long tLong = Convert.ToInt64(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tLong);
+                        break;
+                    case "ulong":
+                        Console.Write("Enter value: ");
+                        long tULong = Convert.ToInt64(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tULong);
+                        break;
+                    case "float":
+                        Console.Write("Enter value: ");
+                        float tFloat = Convert.ToSingle(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tFloat);
+                        break;
+                    case "double":
+                        Console.Write("Enter value: ");
+                        double tDouble = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tDouble);
+                        break;
+                    case "decimal":
+                        Console.Write("Enter value: ");
+                        decimal tDecimal = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine("Success! Value: " + tDecimal);
+                        break;
+                    case "char":
+                        Console.Write("Enter value: ");
+                        char tChar = Convert.ToChar(Console.ReadLine());
+                        Console.WriteLine("Success! Value: \'" + tChar + "\'");
+                        break;
+                    case "string":
+                        Console.Write("Enter value: ");
+                        string tString = Console.ReadLine();
+                        Console.WriteLine("Success! Value: \"" + tString + "\"");
+                        break;
+                    default: throw new Exception("Unknown type.");
+                }
+
+            }
+            catch (Exception ex) { Console.WriteLine("Error! " + ex.Message + " Please try again."); }
+        }
+        static void Main(string[] args)
+        {
+           while(true)
+            {
+                Console.Write("Enter data type: ");
+                InputValue(Console.ReadLine());
+                Console.Write("Continue? (Y/N): ");
+                if (Console.ReadLine() != "Y") {break;};
+            }
+        }
+    }
+}
