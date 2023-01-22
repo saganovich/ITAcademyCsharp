072a13a2e1b839dfef81b58f6362d30dcee8d453
saganovich <saganovich.alexander@yandex.by>
2023-01-22 23:12:42 +0300

    Add Converter

---
+++ L2_Converter/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace L2_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter distance in centimeters: ");
+                    string distanceCm = Console.ReadLine();
+                    int distanceM = (int)(Convert.ToDouble(distanceCm) / 100);
+                    Console.WriteLine($"There are {distanceM} whole meters in {distanceCm} centimeters.");
+                    Console.WriteLine("Continue? (Y/N)");
+                    if (Console.ReadLine() != "Y") { break;}
+                }
+                catch (Exception ex) { Console.WriteLine("Error! " + ex.Message + " Please try again."); }
+            }
+        }
+    }
+}
