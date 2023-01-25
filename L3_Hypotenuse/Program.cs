298636cc7ffbd8524160f25c2e822dc9e2671aa2
saganovich <saganovich.alexander@yandex.by>
2023-01-25 22:39:42 +0300

    Initialize commit

---
+++ L3_Hypotenuse/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace L3_Hypotenuse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the length of the first leg:");
+
+                    double firstLeg = Convert.ToDouble(Console.ReadLine());
+
+                    if (firstLeg < 0) { throw new Exception("Number must be positive."); }
+
+                    Console.WriteLine("Enter the length of the second leg:");
+
+                    double secondLeg = Convert.ToDouble(Console.ReadLine());
+
+                    if (secondLeg < 0) { throw new Exception("Number must be positive."); }
+                    
+                    double hypotenuse = Math.Round(Math.Sqrt(Math.Pow(firstLeg, 2) + Math.Pow(secondLeg, 2)), 3);
+
+                    Console.WriteLine($"The hypotenuse equals {hypotenuse}.");
+
+                    break;
+                }
+                catch (Exception ex) { Console.WriteLine("Error! " + ex.Message + " Please try again."); }
+            }
+        }
+    }
+}
