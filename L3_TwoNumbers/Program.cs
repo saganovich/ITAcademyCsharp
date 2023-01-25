62d43e669b801792c9b9b11235f8f7b39cc2037a
saganovich <saganovich.alexander@yandex.by>
2023-01-25 22:41:36 +0300

    Initialize commit

---
+++ L3_TwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace L3_TwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the first number:");
+                int firstNum = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the second number:");
+                int secondNum = Convert.ToInt32(Console.ReadLine());
+
+                if (firstNum >= 0 && firstNum <= 10 && secondNum >= 0 && secondNum <= 10)
+                {
+                    Console.WriteLine($"The product equals {firstNum * secondNum}.");
+                    break;
+                }
+                Console.WriteLine("Numbers are invalid! Please try again.");
+            }
+        }
+    }
+}
