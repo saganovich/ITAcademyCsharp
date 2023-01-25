0b80abcd6c16bae0d086d85b73a9faf7e27f328c
saganovich <saganovich.alexander@yandex.by>
2023-01-25 22:41:08 +0300

    Initialize commit

---
+++ L3_MultiplicationTable/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace L3_MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter table size:");
+            uint tableSize = Convert.ToUInt32(Console.ReadLine());
+            for (int i = 1; i <= tableSize; i++)
+            {
+                for (int j = 1; j <= tableSize; j++)
+                {
+                    Console.Write(i * j + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
