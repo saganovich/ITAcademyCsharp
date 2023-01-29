5aa238a6776bc13cfdab532f366e24f281be15de
saganovich <saganovich.alexander@yandex.by>
2023-01-29 21:50:06 +0300

    Task with sum & average

---
+++ L4_AverageAndSum/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace L4_AverageAndSum
+{
+    internal class Program
+    {
+        static bool TryParse(out uint number)
+        {
+            string sNumber = Console.ReadLine();
+            if (UInt32.TryParse(sNumber, out number))
+            {
+                Console.WriteLine();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Error! You must enter a positive integer. Please try again.\n");
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a:");
+                if (!TryParse(out uint a)) { continue; }
+                Console.WriteLine("Enter b:");
+                if (!TryParse(out uint b)) { continue; }
+                if (b < a) { (a, b) = (b, a); }
+                uint sum = 0;
+                int numCounter = 0;
+                for (uint i = a; i <= b; i++)
+                {
+                    sum += i;
+                    numCounter++;
+                }
+                decimal avg = sum / (decimal)numCounter;
+                Console.WriteLine($"Interval [{a};{b}]\nSum = {sum}\nAverage = {Math.Round(avg, 2)}\n");
+                Console.Write("Continue? (Y/N): ");
+                if (Console.ReadLine() != "Y") { break; }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
