c045630ca89fd6b6a6d851a048bfb2ee1b83bd46
saganovich <saganovich.alexander@yandex.by>
2023-01-29 20:29:17 +0300

    Task for a cycle with a counter

---
+++ L4_For/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace L4_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 100; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
