627d34b9c62c4f3b6efa6f403008fb33dadb2a2a
saganovich <saganovich.alexander@yandex.by>
2023-01-29 20:15:43 +0300

    Task for a cycle with a post condition

---
+++ L4_DoWhile/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace L4_DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int curNum = 0;
+            do
+            {
+                curNum -= 20;
+                Console.WriteLine(curNum);
+            } 
+            while (curNum > -100);
+        }
+    }
+}
