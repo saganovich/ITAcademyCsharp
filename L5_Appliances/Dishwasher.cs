061b722d85778188f10b5871896f2b8aa6799261
saganovich <saganovich.alexander@yandex.by>
2023-02-08 00:50:19 +0300

    Init task with appliances

---
+++ L5_Appliances/Dishwasher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5_Appliances
+{
+    internal class Dishwasher : Appliances
+    {
+        public Dishwasher(string brand, string model, double powerCons, decimal recomendedPrice, double capacity)
+        {
+            this.type = "Dishwasher";
+            this.brand = brand;
+            this.model = model;
+            this.pwrCons = powerCons;
+            this.recomendPrice = recomendedPrice;
+            this.capacity = capacity;
+            Random rnd = new Random();
+            this.isBroken = rnd.Next(4) == 1;
+        }
+        public override void Work()
+        {
+            Console.WriteLine($"{type} machine washing dishes.");
+        }
+    }
+}
---
+++ L5_Appliances/WashMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5_Appliances
+{
+    internal class WashMachine : Appliances
+    {
+        public WashMachine(string brand, string model, double powerCons, decimal recomendedPrice,double capacity)
+        {
+            this.type = "Washing machine";
+            this.brand = brand;
+            this.model = model;
+            this.pwrCons = powerCons;
+            this.recomendPrice = recomendedPrice;
+            this.capacity = capacity;
+            Random rnd = new Random();
+            this.isBroken = rnd.Next(2) == 1;
+        }
+        public override void Work()
+        {
+            Console.WriteLine($"{type} washing clothes.");
+        }
+    }
+}
