0db989c5818b1582f6170e6e36630a7a492760e2
saganovich <saganovich.alexander@yandex.by>
2023-02-07 08:42:44 +0300

    Init task company & workers

---
+++ L5_Company/Company.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5_Company
+{
+    internal class Company
+    {
+        private string name;
+        private string adress;
+        private List<Worker> workers;
+        public string Name
+        {
+            get => name; set => name = value;
+        }
+        private string Adress
+        {
+            get => adress; set => adress = value;
+        }
+        public int CountWorkers
+        {
+            get => workers.Count;
+        }
+        public void Recruit(Worker recruiter, Post post, decimal salary)
+        {
+            workers.Add(recruiter);
+            recruiter.Post = post;
+            recruiter.Salary = salary;
+
+        }
+        public void Dismiss(Worker recruiter)
+        {
+            recruiter.Post = Post.None;
+            recruiter.Salary = 0;
+            workers.Remove(recruiter);
+        }
+
+        public void SetPost(Worker worker, Post post)
+        {
+            worker.Post = post;
+        }
+        public void SetSalary(Worker worker, decimal salary)
+        {
+            worker.Salary = salary;
+        }
+        public string GetMainInfo()
+        {
+            string tempStr = string.Empty;
+            tempStr += $"Company name: {this.Name}\nAdress: {this.Adress}\nCount workers: {this.CountWorkers}\n\nWorkers:\n";
+            for (int i = 0; i < workers.Count; i++)
+            {
+                tempStr += $"[{i}]\t{this.workers[i].FirstName + " " + this.workers[i].LastName}\t|{this.workers[i].Age}|\t{this.workers[i].Post}|{this.workers[i].Salary}$\n";
+            }
+            tempStr += "---";
+            return tempStr;
+        }
+        public Worker GetWorkerByIndex(int index)
+        {
+            if (index >= workers.Count) { return null; }
+            return workers[index];
+        }
+        public Company(string name, string adress)
+        {
+            this.name = name;
+            this.adress = adress;
+            this.workers = new List<Worker>();
+        }
+        public enum Post
+        {
+            None = 0,
+            Staff = 1,
+            Assistant = 2,
+            SeniorManager = 3,
+            TechnicalManager = 4,
+            CEO = 5
+        }
+    }
+}
---
+++ L5_Company/Worker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5_Company
+{
+    internal class Worker
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private Company.Post post;
+        private decimal salary;
+        public string FirstName
+        {
+            get => firstName; set => firstName = value;
+        }
+        public string LastName
+        {
+            get => lastName; set => lastName = value;
+        }
+        public int Age
+        {
+            get => age; set => age = value;
+        }
+        public Company.Post Post
+        {
+            get => post;
+            internal protected set => post = value;
+        }
+        public decimal Salary
+        {
+            get => salary;
+            internal protected set => salary = value;
+        }
+        public Worker(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+    }
+}
