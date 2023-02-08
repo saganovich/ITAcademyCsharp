fcac0618df6857cf361c884a3f09c51ba1c371c1
Alexander <121257503+saganovich@users.noreply.github.com>
2023-02-08 22:52:14 +0300

    Init task with point

---
+++ L5_Point/Point.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5_Point
+{
+    internal class Point
+    {
+        private double coordX;
+        private double coordY;
+        private static uint count = 0;
+        public static uint Count => count;
+        public double X
+        {
+            get => coordX;
+            set => coordX = value;
+        }
+        public double Y
+        {
+            get => coordY;
+            set => coordY = value;
+        }
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+            count++;
+        }
+        public static double LengthPoints(Point pnt1, Point pnt2)
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(pnt1.coordX - pnt2.coordX, 2) + Math.Pow(pnt1.coordY - pnt2.coordY, 2)), 3);
+        }
+        public static double LengthPoints(double x1, double y1, double x2, double y2)
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)), 3);
+        }
+    }
+
+
+}
---
+++ L5_Point/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace L5_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Point.Count);
+            Point pnt1 = new Point(-10, 10);
+            Point pnt2 = new Point(5, 9);
+            Point pnt3 = new Point(14, 14);
+            Console.WriteLine(Point.LengthPoints(pnt1, pnt2));
+            pnt2.X = 17;
+            Console.WriteLine(Point.LengthPoints(pnt1, pnt2));
+            Console.WriteLine(Point.LengthPoints(10, 15, 12, 4));
+            Console.WriteLine(Point.Count);
+        }
+    }
+}
