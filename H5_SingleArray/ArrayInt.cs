b61fcc8a1c721ac673fca106112be6e7e5ebaa4a
saganovich <saganovich.alexander@yandex.by>
2023-02-12 21:36:36 +0300

    Single array task

---
+++ H5_SingleArray/ArrayInt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H5_SingleArray
+{
+	static class ArrayInt
+	{
+		public static void SortDesc(int[] array)
+		{
+			Array.Sort(array);
+			Array.Reverse(array);
+		}
+		public static void PrintArray(int[] array, int mode)
+		{
+			switch (mode)
+			{
+				case 1:
+					for (int i = 0; i < array.Length; i++)
+					{
+						Console.Write(array[i] + " ");
+					}
+					Console.WriteLine();
+					break;
+				case 2:
+					foreach (int elem in array)
+					{
+						Console.Write(elem + " ");
+					}
+					Console.WriteLine();
+					break;
+			}
+		}
+		public static void PrintLastElems(int[] array)
+		{
+			for (int i = array.Length - 3; i < array.Length; i++)
+			{
+				Console.Write(array[i] + " ");
+			}
+			Console.WriteLine();
+		}
+		public static void PrintEvNumberMoreFive(int[] array)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i] > 5 && array[i] % 2 == 0)
+				{
+					Console.Write(array[i] + " ");
+				}
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ H5_SingleArray/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace H5_SingleArray
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = new int[6];
+			for (int i = 0; i < array.Length; i++)
+			{
+				while (!InputValue(i, out array[i])) ;
+			}
+			ArrayInt.SortDesc(array);
+			ArrayInt.PrintArray(array, 1);
+			ArrayInt.PrintArray(array, 2);
+			ArrayInt.PrintLastElems(array);
+			ArrayInt.PrintEvNumberMoreFive(array);
+		}
+
+		static bool InputValue(int index, out int value)
+		{
+			Console.Write($"Enter array[{index}]:");
+			if (Int32.TryParse(Console.ReadLine(), out value))
+			{
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("Error!An integer value was expected.Please try again");
+				return false;
+			}
+		}
+	}
+}
