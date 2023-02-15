36a6eb27c13202bd86f76cc0ef98d83b5b80401d
Alexander <121257503+saganovich@users.noreply.github.com>
2023-02-15 20:07:17 +0300

    Renamed dir

---
+++ Homework 5/H5_DoubleArray/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace H5_DoubleArray
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[,] array = new int[3, 3];
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					array[i, j] = Randomize.Get();
+				}
+			}
+			array.PrintArray();
+			Console.WriteLine();
+			for(int i = 0; i < 3; i++)
+			{
+				Console.Write(array.GetMaxInRow(i)+" ");
+			}
+
+
+		}
+
+	}
+}
---
+++ Homework 5/H5_DoubleArray/Randomize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H5_DoubleArray
+{
+	internal static class Randomize
+	{
+		public static int Get()
+		{
+			Random rnd = new Random();
+			return rnd.Next(0, 9);
+		}
+	}
+}
