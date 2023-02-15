705ece11c4352d56a30b59e6cedce399faf134f5
Alexander <121257503+saganovich@users.noreply.github.com>
2023-02-15 20:25:14 +0300

    rework

---
+++ Homework 5/DoubleArray/ArrExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleArray
+{
+	internal static class ArrExt
+	{
+		public static void Print(this int[] arr)
+		{
+			int columns = arr.GetUpperBound(0) + 1;
+			for (int i = 0; i < columns; i++)
+			{
+				Console.Write(arr[i] + " ");
+			}
+		}
+	}
+}
--- Homework 5/DoubleArray/Program.cs
+++ Homework 5/DoubleArray/Program.cs
@@ -5,21 +5,21 @@
 		static void Main(string[] args)
 		{
 			int[,] array = new int[3, 3];
+			int[] maxInRow = new int[3];
 			for (int i = 0; i < 3; i++)
 			{
+				int max = int.MinValue;
 				for (int j = 0; j < 3; j++)
 				{
 					array[i, j] = Randomize.Get();
+					Console.Write(array[i, j] + " ");
+					if(array[i, j] > max) { max = array[i, j]; }
 				}
+				maxInRow[i] = max;
+				Console.WriteLine();
 			}
-			array.PrintArray();
 			Console.WriteLine();
-			for(int i = 0; i < 3; i++)
-			{
-				Console.Write(array.GetMaxInRow(i)+" ");
-			}
-
-
+			maxInRow.Print();
 		}
 
 	}
