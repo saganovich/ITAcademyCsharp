80d970fdc722557c3b69760575c7b8f4b37c6b6b
Alexander <121257503+saganovich@users.noreply.github.com>
2023-02-15 21:38:31 +0300

    Word counter

---
+++ Homework 5/WordCounter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WordCounter
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string text = Console.ReadLine();
+			char[] marks = { ' ', ',', '.', '-', ':', ';', '!', '?' };
+			string[] words = text.Split(marks, StringSplitOptions.RemoveEmptyEntries);
+			Console.WriteLine(words.Length);
+		}
+	}
+}
