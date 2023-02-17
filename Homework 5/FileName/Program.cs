722f0964d0307380ea1b8c747ff6f5e737025f6b
saganovich <saganovich.alexander@yandex.by>
2023-02-17 15:22:29 +0300

    rework

--- Homework 5/FileName/Program.cs
+++ Homework 5/FileName/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace FileName
+namespace FileName	
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
 			string path = @"c:\WebServers\home\testsite\www\hello\myfile.txt";
-			Regex regex = new Regex(@"\w*[.]\w*$");
+			Regex regex = new Regex(@"[\w\. ]+(?=[\.])");
 			Console.WriteLine(regex.Match(path));
+			Console.WriteLine(Path.GetFileNameWithoutExtension(path));
 		}
 	}
 }
