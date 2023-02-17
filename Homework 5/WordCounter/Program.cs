e01adfaf7a3026d344e5e6785b0562ca5b5f0340
saganovich <saganovich.alexander@yandex.by>
2023-02-17 13:46:40 +0300

    add method with regex

--- Homework 5/WordCounter/Program.cs
+++ Homework 5/WordCounter/Program.cs
@@ -8,9 +8,14 @@ namespace WordCounter
 		static void Main(string[] args)
 		{
 			string text = Console.ReadLine();
+			
 			char[] marks = { ' ', ',', '.', '-', ':', ';', '!', '?' };
 			string[] words = text.Split(marks, StringSplitOptions.RemoveEmptyEntries);
-			Console.WriteLine(words.Length);
+			Console.WriteLine($"string.Split result: {words.Length}");
+			Regex regex = new Regex(@"\w+\s*");
+			MatchCollection mCol = regex.Matches(text);
+			Console.WriteLine($"Regex.Matches result: {words.Length}");
+
 		}
 	}
 }
