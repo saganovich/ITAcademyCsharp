0ed5c4b120fa16f91623573d6504121ad30ca331
saganovich <saganovich.alexander@yandex.by>
2023-02-17 13:59:06 +0300

    FileName task

---
+++ Homework 5/FileName/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FileName
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string path = @"c:\WebServers\home\testsite\www\hello\myfile.txt";
+			Regex regex = new Regex(@"\w*[.]\w*$");
+			Console.WriteLine(regex.Match(path));
+		}
+	}
+}
