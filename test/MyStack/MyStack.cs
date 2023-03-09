781320a7458d3cc0be178ff516e3f8cbb0a4003a
Alexander <121257503+saganovich@users.noreply.github.com>
2023-03-09 17:28:06 +0300

    init task

---
+++ test/MyStack/MyStack.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+	sealed class MyStack <T>
+	{
+		private int top;
+		private int size;
+		private T[] array;
+		public int Size
+		{
+			get
+			{
+				return array.Length;
+			}
+			private set
+			{
+				size = value;
+			}
+		}
+		
+		public MyStack()
+		{
+			this.size = 0;
+			this.array = new T[size];
+		}
+
+		public void Push(T item)
+		{
+			Resize(TypeResize.Up);
+			array[Size-1] = item;
+		}
+		public T Pop()
+		{
+			if (Size == 0) { throw new Exception("The stack is empty"); }
+			T tempElem = this.array[Size-1];
+			Resize(TypeResize.Down);
+			return tempElem;
+		}
+		public T Peek()
+		{
+			if (Size == 0) { throw new Exception("The stack is empty"); }
+			return this.array[Size-1];
+		}
+		private void Resize(TypeResize type)
+		{
+			switch (type)
+			{
+				case TypeResize.Up:
+					Array.Resize(ref this.array, Size+1);
+					break;
+				case TypeResize.Down:
+					Array.Resize(ref this.array, Size-1);
+					break;
+			}
+			
+		}
+		public override string ToString()
+		{
+			StringBuilder tempstr = new StringBuilder();
+			tempstr.Append("[");
+			foreach(T item in this.array)
+			{
+				tempstr.Append("\"");
+				tempstr.Append(item.ToString());
+				tempstr.Append("\",");
+			}
+			if(this.array.Length > 0)tempstr.Remove(tempstr.Length-1,1);
+			tempstr.Append("]");
+			return tempstr.ToString();
+		}
+	}
+}
---
+++ test/MyStack/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MyStack
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Создание int стека..");
+            MyStack<int> stack = new MyStack<int>();
+			Console.WriteLine($"Размер стека : {stack.Size}");
+            Console.WriteLine("Добавление 10 в стек");
+            stack.Push(10);
+			Console.WriteLine("Добавление 20 в стек");
+			stack.Push(20);
+			Console.WriteLine("Добавление 30 в стек");
+			stack.Push(30);
+			Console.WriteLine("Добавление 40 в стек");
+			stack.Push(40);
+			Console.WriteLine("Добавление 50 в стек");
+			stack.Push(50);
+			Console.WriteLine($"Данные в стеке: {stack}");
+			Console.WriteLine($"Размер стека : {stack.Size}");
+			Console.WriteLine($"Просмотр первого элемента стека: {stack.Peek()}");
+			Console.WriteLine($"Размер стека : {stack.Size}");
+			Console.WriteLine($"Извлечение первого элемента стека: {stack.Pop()}");
+			Console.WriteLine($"Данные в стеке: {stack}");
+			Console.WriteLine($"Извлечение первого элемента стека: {stack.Pop()}");
+			Console.WriteLine($"Извлечение первого элемента стека: {stack.Pop()}");
+			Console.WriteLine($"Извлечение первого элемента стека: {stack.Pop()}");
+			Console.WriteLine($"Извлечение первого элемента стека: {stack.Pop()}");
+			Console.WriteLine($"Данные в стеке: {stack}");
+		}
+	}
+}
